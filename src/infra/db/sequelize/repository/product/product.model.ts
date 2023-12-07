175fbe7adeb7bf979fcbe488d0a0244e043be26b
Eduardy Lopes <eduardylopes@gmail.com>
2023-12-06 23:44:39 -0300

    feat(sequelize): implement product repository

---
+++ src/infra/db/sequelize/repository/product/product.model.ts
@@ -0,0 +1,23 @@
+import {
+  Table,
+  Model,
+  PrimaryKey,
+  Column,
+  DataType,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'products',
+  timestamps: false,
+})
+export default class ProductModel extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.STRING })
+  declare id: string;
+
+  @Column({ allowNull: false, type: DataType.STRING })
+  declare name: string;
+
+  @Column({ allowNull: false, type: DataType.NUMBER })
+  declare price: number;
+}
