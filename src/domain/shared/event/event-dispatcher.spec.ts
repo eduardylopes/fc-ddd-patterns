1fc01565a72681c3d6104c8fe316a235e9c5efa1
Eduardy Lopes <eduardylopes@gmail.com>
2023-12-06 22:55:30 -0300

    feat(shared): implement event dispatcher

---
+++ src/domain/shared/event/event-dispatcher.spec.ts
@@ -0,0 +1,81 @@
+import { describe, expect, it, vi } from 'vitest';
+import SendEmailWhenProductIsCreatedHandler from '../../product/event/handler/send-email-when-product-is-created.handler';
+import ProductCreatedEvent from '../../product/event/product-created.event';
+import EventDispatcher from './event-dispatcher';
+
+describe('Domain events tests', () => {
+  it('should be able to register an event handler', () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent']
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent'].length).toBe(
+      1
+    );
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]
+    ).toMatchObject(eventHandler);
+  });
+
+  it('should be able to unregister an event handler', () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]
+    ).toMatchObject(eventHandler);
+
+    eventDispatcher.unregister('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent']
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent'].length).toBe(
+      0
+    );
+  });
+
+  it('should be able to unregister all event handlers', () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]
+    ).toMatchObject(eventHandler);
+
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent']
+    ).toBeUndefined();
+  });
+
+  it('should be able to notify all event handlers', () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = vi.spyOn(eventHandler, 'handle');
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers['ProductCreatedEvent'][0]
+    ).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: 'Product 1',
+      description: 'Product 1 description',
+      price: 10.0,
+    });
+
+    eventDispatcher.notify(productCreatedEvent);
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+});
